e712c4d321236a69522a463e5559932de4f4bd5e
carolinecullinan <c.cullinan@student.tudelft.nl>
2022-11-22 00:45:25 +0100

    Outro

---
+++ src/lib/Outro.svelte
@@ -0,0 +1,44 @@
+<script>
+</script>
+
+<div class="introParagraph">
+    <p>But why the discrepancy? Whether it be ink <span style = 'color: #7695c4; font-weight: 700'>pens</span>, packs of <span style = 'color: #7695c4; font-weight: 700'>rice</span>, tubes of <span style = 'color: #7695c4; font-weight: 700'>toothpaste</span>, or <span style = 'color: #7695c4; font-weight: 700'>15-minute telephone calls</span>, prison prices and the amount of labor required to be able to afford these prices clearly differ across states. Not because ink <span style = 'color: #7695c4; font-weight: 700'>pens</span>, packs of <span style = 'color: #7695c4; font-weight: 700'>rice</span>, tubes of <span style = 'color: #7695c4; font-weight: 700'>toothpaste</span>, or <span style = 'color: #7695c4; font-weight: 700'>15-minute telephone calls</span> are more or less expensive in absolute across states, but because a lack of external oversight encourages prisons and their conditions to vary.<br><br>And, in recent years, while some states have created external independent entities that monitor and report on statewide conditions inside correctional facilities, other states have not. This basically means that some states subject correctional facilities to improved transparency and increased accountability, while others don’t. So, different prison conditions, related to wages, working conditions, and even commissary prices, can exist in different states, thus contributing to even further inequality within the US prison system.
+    </p>
+    <p>Thus, as demonstrated by this piece, all incarcerated people in all states are not only subjected to unfair labor practices when compared to non-incarcerated people in their same state, but they are also subjected to inequitable compensation when compared to other incarcerated people employed in other states. <br><br>So, what’s a dollar cost for folks subjected to US incarceration?<br><br>That greatly and unjustly depends on the state in which someone is experiencing incarceration.</p>
+    <br>
+    <!-- <br>
+    by Caroline Cul -->
+</div>
+
+<style>
+    .title {
+        height: 60vh;
+        display: flex;
+        place-items: center;
+        flex-direction: column;
+        justify-content: center;
+        text-align: center;;
+        margin-bottom: 60px;
+        max-width: 650px;
+        margin:auto
+    }
+
+    h2{
+        font-weight: 400;
+    }
+
+    .introParagraph {
+        text-align: justify;
+        max-width: 500px;
+        margin: auto;
+    }
+
+    .centerParagraph {
+        text-align: center;
+    }
+
+    .material-symbols-outlined {
+        font-variation-settings: "FILL" 0, "wght" 700, "GRAD" 0, "opsz" 48;
+        font-size: 48px
+    }
+</style>
